0f85bdb5c8d222b418b46eb97715a7b8626d2528
James Beal <zz9pzza@gmail.com>
2012-10-19 21:16:08 +0100

    Lets add the preferences to the preferences table

---
+++ db/migrate/20121019194026_add_mobile_downloads_to_prefs.rb
@@ -0,0 +1,15 @@
+class AddUserKindleEmailPrefs < ActiveRecord::Migration
+  def self.up
+    add_column :preferences, :download_email_address, :string
+    add_column :preferences, :download_preffered_format, :string
+    add_column :preferences, :download_activation_key, :string
+    add_column :preferences, :download_activated, :boolean, :default => false, :null => false
+  end
+
+  def self.down
+    remove_column :preferences, :download_email_address
+    remove_column :preferences, :download_preffered_format
+    remove_column :preferences, :download_activation_key
+    remove_column :preferences, :download_activated
+  end
+end
