c7c6691afc3bd545bfd75d7e0c0b3ac26d8e43a2
james_ <zz9pzza@gmail.com>
2014-10-19 16:46:01 +0000

    And admin users

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,19 @@
 class Admin < ActiveRecord::Base
   # Authlogic gem
   acts_as_authentic do |config|
+    config.crypto_provider = Authlogic::CryptoProviders::BCrypt
     config.transition_from_restful_authentication = true
-    config.transition_from_crypto_providers = Authlogic::CryptoProviders::Sha1
+    config.transition_from_crypto_providers = [Authlogic::CryptoProviders::Sha512, Authlogic::CryptoProviders::Sha1]
+    config.validates_length_of_password_field_options = {:on => :update,
+                                                         :minimum => ArchiveConfig.PASSWORD_LENGTH_MIN,
+                                                         :if => :has_no_credentials?}
+    config.validates_length_of_password_confirmation_field_options = {:on => :update,
+                                                                      :minimum => ArchiveConfig.PASSWORD_LENGTH_MIN,
+                                                                      :if => :has_no_credentials?}
+  end
+
+  def has_no_credentials?
+    self.crypted_password.blank?
   end
   
   has_many :log_items
