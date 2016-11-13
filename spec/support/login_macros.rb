b5395222beddbc5308e277af2f2b2b614b10d58d
zz9pzza <zz9pzza@gmail.com>
2016-11-13 23:55:18 +0000

    Make fake_login drier

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -2,10 +2,7 @@ module LoginMacros
 
   def fake_login
     # Stub out the current_user method
-    @current_user = FactoryGirl.create(:user)
-    allow(controller).to receive(:logged_in?).and_return(true)
-    allow(controller).to receive(:current_user).and_return(@current_user)
-    allow(controller).to receive(:logout_if_not_user_credentials).and_return(nil)
+    fake_login_known_user(FactoryGirl.create(:user))
   end
   
   def fake_login_known_user(user)
