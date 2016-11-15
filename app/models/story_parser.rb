fa258379a5fe2eb901f1763946fb333ac652c44d
zz9pzza <zz9pzza@gmail.com>
2016-11-15 23:07:11 +0000

    Use old method falling back to new method for current user

--- app/models/story_parser.rb
+++ app/models/story_parser.rb
@@ -281,8 +281,9 @@ class StoryParser
       end
 
       # set authors for the works
+      current_user = User.current_user || @current_user
       pseuds = []
-      pseuds << @current_user.default_pseud unless options[:do_not_set_current_author] || @current_user.nil?
+      pseuds << current_user.default_pseud unless options[:do_not_set_current_author] || @current_user.nil?
       pseuds << options[:archivist].default_pseud if options[:archivist]
       pseuds += options[:pseuds] if options[:pseuds]
       pseuds = pseuds.uniq
@@ -307,7 +308,7 @@ class StoryParser
               # we're not allowed to import works from this address
               raise Error, "Author #{external_author_name.name} at #{external_author_name.external_author.email} does not allow importing their work to this archive."
             end
-            work.external_creatorships.build(external_author_name: external_author_name, archivist: (options[:archivist] || @current_user))
+            work.external_creatorships.build(external_author_name: external_author_name, archivist: (options[:archivist] || current_user))
           end
         end
       end
