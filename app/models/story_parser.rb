4cc97ae14eae894ab8b34fe8ab24c283113e9b56
zz9pzza <zz9pzza@gmail.com>
2018-04-29 00:21:46 +0100

    Classes in strings.....

--- app/models/story_parser.rb
+++ app/models/story_parser.rb
@@ -8,13 +8,13 @@ class StoryParser
   require 'open-uri'
   include HtmlCleaner
 
-  OPTIONAL_META = { notes: 'Note',
-                    freeform_string: 'Tag',
-                    fandom_string: 'Fandom',
-                    rating_string: 'Rating',
-                    warning_string: 'Warning',
-                    relationship_string: 'Relationship|Pairing',
-                    character_string: 'Character' }.freeze
+  OPTIONAL_META = {notes: 'Note',
+                   freeform_string: 'Tag',
+                   fandom_string: 'Fandom',
+                   rating_string: 'Rating',
+                   archivewarning_string: 'Warning',
+                   relationship_string: 'Relationship|Pairing',
+                   character_string: 'Character' }.freeze
   REQUIRED_META = { title: 'Title',
                     summary: 'Summary',
                     revised_at: 'Date|Posted|Posted on|Posted at',
@@ -135,7 +135,7 @@ class StoryParser
   # :do_not_set_current_author - true means do not save the current user as an author
   # :importing for others - true means try and add external author for the work
   # :pseuds - a list of pseuds to set as authors
-  # :set_tags, :fandom, :rating, :warning, :character, :relationship - sets these tags
+  # :set_tags, :fandom, :rating, :archivearning, :character, :relationship - sets these tags
   # :override_tags - set tag values even if some were parsed out of the work
   # :post_without_preview - if true, mark the story as posted without previewing
   #
@@ -297,7 +297,7 @@ class StoryParser
     # set default values for required tags
     work.fandom_string = meta_or_default(work.fandom_string, options[:fandom], ArchiveConfig.FANDOM_NO_TAG_NAME)
     work.rating_string = meta_or_default(work.rating_string, options[:rating], ArchiveConfig.RATING_DEFAULT_TAG_NAME)
-    work.archivewarning_strings = meta_or_default(work.warning_strings, options[:warning], ArchiveConfig.WARNING_DEFAULT_TAG_NAME)
+    work.archivewarning_strings = meta_or_default(work.archivewarning_strings, options[:archivewarning], ArchiveConfig.WARNING_DEFAULT_TAG_NAME)
     work.category_string = meta_or_default(work.category_string, options[:category], [])
     work.character_string = meta_or_default(work.character_string, options[:character], [])
     work.relationship_string = meta_or_default(work.relationship_string, options[:relationship], [])
@@ -861,9 +861,9 @@ class StoryParser
   # Additional processing for meta - currently to make sure warnings
   # that aren't Archive warnings become additional tags instead
   def post_process_meta(meta)
-    if meta[:warning_string]
-      result = process_warnings(meta[:warning_string], meta[:freeform_string])
-      meta[:warning_string] = result[:warning_string]
+    if meta[:archivewarning_string]
+      result = process_warnings(meta[:archivewarning_string], meta[:freeform_string])
+      meta[:archivewarning_string] = result[:archivewarning_string]
       meta[:freeform_string] = result[:freeform_string]
     end
     meta
@@ -871,11 +871,11 @@ class StoryParser
 
   def process_warnings(warning_string, freeform_string)
     result = {
-      warning_string: warning_string,
-      freeform_string: freeform_string
+        archivewarning_string: warning_string,
+        freeform_string: freeform_string
     }
     new_warning = ''
-    result[:warning_string].split(/\s?,\s?/).each do |warning|
+    result[:archivewarning_string].split(/\s?,\s?/).each do |warning|
       if ArchiveWarning.warning? warning
         new_warning += ', ' unless new_warning.blank?
         new_warning += warning
@@ -883,7 +883,7 @@ class StoryParser
         result[:freeform_string] = (result[:freeform_string] || '') + ", #{warning}"
       end
     end
-    result[:warning_string] = new_warning
+    result[:archivewarning_string] = new_warning
     result
   end
 
