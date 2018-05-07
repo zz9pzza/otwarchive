60bb8ab2c8b1e7deb593686077b88f76a5a3bc35
zz9pzza <zz9zza@gmail.com>
2018-05-07 18:36:10 +0000

    add migration back

---
+++ db/migrate/20180507183131_change_warning_to_archive_warning.rb
@@ -0,0 +1,19 @@
+class ChangeWarningToArchiveWarning < ActiveRecord::Migration[5.1]
+  def up
+    execute "UPDATE tags SET type = 'ArchiveWarning' WHERE type = 'Warning'"
+    rename_column :prompt_restrictions, :warning_num_allowed, :archivewarning_num_allowed
+    rename_column :prompt_restrictions, :warning_num_required, :archivewarning_num_required
+    rename_column :prompt_restrictions, :require_unique_warning, :require_unique_archivewarning
+    rename_column :prompt_restrictions, :allow_any_warning, :allow_any_archivewarning
+    rename_column :prompts, :any_warning, :any_archivewarning
+  end
+
+  def down
+    execute "UPDATE tags SET type = 'Warning' WHERE type = 'ArchiveWarning'"
+    rename_column :prompt_restrictions, :archivewarning_num_allowed ,:warning_num_allowed
+    rename_column :prompt_restrictions, :archivewarning_num_required, :warning_num_required
+    rename_column :prompt_restrictions, :require_unique_archivewarning, :require_unique_warning
+    rename_column :prompt_restrictions, :allow_any_archivewarning, :allow_any_warning
+    rename_column :prompts, :any_archivearning,  :any_warning
+  end
+end
