0e9ce230d788276352963b9fe408eda34591f7f0
zz9pzza <zz9pzza@gmail.com>
2019-05-08 19:08:06 +0000

    Missed a bit

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -49,6 +49,6 @@ class InboxController < ApplicationController
 
   # Allow flexible params through, since we're not posting any data
   def filter_params
-    params.permit(:user_id,filters: [:read, :replied_to, :date])
+    params.permit(:user_id, :utf8, :commit, filters: [:read, :replied_to, :date])
   end
 end
